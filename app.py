7ecb7dde33653fcaa1e521133ad1cdfef8dc3544
Neha <Nehamandwal123@gmail.com>
2025-01-15 15:42:28 +0530

    Decision tree Classifier

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-print ("Hellow World !")
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+
+iris = load_iris()
+data = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+data['target'] = iris.target
+
+X= iris.data
+y=iris.target
+
+X_train, X_test, y_train, y_test =train_test_split(X, y,test_size=0.2, random_state=42)
+
+clf= DecisionTreeClassifier(random_state=42)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt 
+plt.figure(figsize = (12,8))
+plot.tree(clf, feature_names=iris.feature_names)
+plt.title("DC MLOPS")
+plt.show()
