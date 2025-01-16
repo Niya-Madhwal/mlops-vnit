d67eab3b9e15a2788f66c1ae5b6679885b028a13
Neha <Nehamandwal123@gmail.com>
2025-01-16 15:13:39 +0530

    test

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ data['target'] = iris.target
 X= iris.data
 y=iris.target
 
-X_train, X_test, y_train, y_test =train_test_split(X, y,test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test =train_test_split(X, y,test_size=0.3, random_state=42)
 
 clf= DecisionTreeClassifier(random_state=42)
 clf.fit(X_train, y_train)
